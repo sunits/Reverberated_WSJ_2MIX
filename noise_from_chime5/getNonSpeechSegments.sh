d9c085968440a0f69d25ca272b4f561f61f873c5
popcornell <cornellsamuele@gmail.com>
2020-01-03 20:12:24 +0100

    fixed missing arrays bug + np.loadtxt bug

--- noise_from_chime5/getNonSpeechSegments.sh
+++ noise_from_chime5/getNonSpeechSegments.sh
@@ -7,6 +7,9 @@ dihard_sad_label_path=$3
 mkdir -p $noise_dest/lists
 for speaker in S02 S09 S03 S04 S05 S06 S07 S08 S12 S13 S16 S17 S18 S19 S20 S22 S23 S24 ;  do
     for unit in 1 2 3 4 5 6; do
+        if   [[ $speaker == S09  &&  $unit ==  5 ]] || [[ $speaker == S05  &&  $unit ==  3 ]] || [[  $speaker == S22  &&  $unit ==  3  ]] ; then
+            continue
+        fi
         python noise_from_chime5/getNonSpeechSegments.py $CHIME5_WAV_BASE  ${speaker}_U0${unit}  $dihard_sad_label_path/${speaker}_U0${unit}.lab $noise_dest || exit 1;
     done
 done
